b475d73a31be35ee7a485eb3fe271b34960475a5
obernalpo <obernalpo@gmail.com>
2023-03-15 14:18:33 +0100

    add dgp

---
+++ src/main/java/cat/aoc/client_pci/clients/dgt/DGTOperacio.java
@@ -0,0 +1,24 @@
+package cat.aoc.client_pci.clients.dgt;
+
+import cat.aoc.client_pci.model.Operacio;
+
+public enum DGTOperacio implements Operacio {
+    DGT_DADES_VEHICLE,
+    DGT_DADES_VEHICLE_SANCIONS,
+    DGT_PERMISOS_CONDUCTOR,
+    DGT_TITULAR_VIA,
+    DGT_SANCIONS_CONDUCTOR,
+    DGT_VEHICLES_CONDUCTOR,
+    DGT_DISTINTIU_MEDIAMBIENTAL;
+
+    @Override
+    public String getCodiProducte() {
+        return "DGT";
+    }
+
+    @Override
+    public String getCodiModalitat() {
+        return this.name();
+    }
+
+}
---
+++ src/main/java/cat/aoc/client_pci/clients/dgt/DGTPeticionBuilder.java
@@ -0,0 +1,28 @@
+package cat.aoc.client_pci.clients.dgt;
+
+import cat.aoc.client_pci.model.exceptions.ClientException;
+import cat.aoc.client_pci.utils.AbstractPeticionBuilder;
+import generated.dgt.PeticioConsultaVehicle;
+
+public class DGTPeticionBuilder extends AbstractPeticionBuilder<DGTOperacio> {
+    public DGTPeticionBuilder(String propertiesPath) throws ClientException {
+        super(propertiesPath);
+    }
+
+    @Override
+    protected Object[] getDatosEspecificos(DGTOperacio operacio) {
+        return switch (operacio) {
+            case DGT_DADES_VEHICLE -> new Object[]{
+                    buildPeticioConsultaVehicle()
+            };
+            default -> new Object[]{};
+        };
+    }
+
+    private PeticioConsultaVehicle buildPeticioConsultaVehicle() {
+        PeticioConsultaVehicle peticioConsultaVehicle = new PeticioConsultaVehicle();
+        peticioConsultaVehicle.setMatricula("AB1234D");
+        return peticioConsultaVehicle;
+    }
+
+}
